d8ed8f60a18af17e1721e7890d3b110c46aa0cf7
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-11-25 13:51:28 +0530

    added password hash and verify tests

---
+++ src/core/password/hash.test.ts
@@ -0,0 +1,34 @@
+import { describe, test, expect } from 'vitest';
+import { hashPassword } from './hash';
+import { hash } from 'crypto';
+
+describe('hashPassword', () => {
+  test('should create a hashed password from a plain text password', async () => {
+    const password = 'my-secret-password';
+
+    // Run the function
+    const result = await hashPassword(password);
+
+    // Assert it didn't return an error
+    expect(result.isOk()).toBe(true);
+
+    // Unwrap the value to check the format
+    const hash = result._unsafeUnwrap();
+
+    expect(hash).toBeTypeOf('string');
+    expect(hash).toContain('$scrypt$');
+  });
+
+  test('should generate unique hashes for the same password', async () => {
+    const password = 'my-secret-passwprd';
+
+    // Run it twice
+    const result1 = await hashPassword(password);
+    const result2 = await hashPassword(password);
+
+    const hash1 = result1._unsafeUnwrap();
+    const hash2 = result2._unsafeUnwrap();
+
+    expect(hash1).not.toEqual(hash2);
+  });
+});
---
+++ src/core/password/verify.test.ts
@@ -0,0 +1,42 @@
+import { describe, test, expect } from 'vitest';
+import { verifyPassword } from './verify';
+import { hashPassword } from './hash';
+
+describe('verifyPassword', () => {
+  test('should return true when the password matches the hash', async () => {
+    const password = 'my-secret-password';
+
+    const hashresult = await hashPassword(password);
+    const validHash = hashresult._unsafeUnwrap();
+
+    const result = await verifyPassword(password, validHash);
+
+    expect(result.isOk()).toBe(true);
+    expect(result._unsafeUnwrap()).toBe(true);
+  });
+
+  test('should return false when password does not match the hash', async () => {
+    const password = 'my-secret-password';
+
+    const hashResult = await hashPassword(password);
+    const validHash = hashResult._unsafeUnwrap();
+
+    const result = await verifyPassword('my-wrong-password', validHash);
+
+    expect(result.isOk()).toBe(true);
+    expect(result._unsafeUnwrap()).toBe(false);
+  });
+
+  test('Should return an error if the hash format is invalid', async () => {
+    const result = await verifyPassword('password', 'invalid-hash-string');
+
+    // Expect the result to be an Error
+    expect(result.isErr()).toBe(true);
+
+    // Unwrap the error value
+    const error = result._unsafeUnwrapErr();
+
+    // Check that we got the specific error we defined in our code
+    expect(error.name).toBe('InvalidPasswordHashFormatError');
+  });
+});
